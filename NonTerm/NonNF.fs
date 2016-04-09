b6ae0a153d98ac9cbd66091927e1a120ddae1ad8
Alexander Fedotov <Alexander Fedotov>
2016-04-09 14:50:28 +0200

    initial commit

---
+++ NonTerm/NonNF.fs
@@ -0,0 +1,86 @@
+﻿module NonNF
+
+type transition = { inp:string; dest:int }
+type state = { stateID:int; transitions: transition list; isFinal:bool }
+
+let getAlphabet (lhses:string list):char list = 
+
+    let rec getAlphabet' (ls:string list):char list = 
+        match ls with
+        | []    -> []
+        | x::xs -> [for s in x do yield s] @ getAlphabet' xs
+
+    Set.toList (set (getAlphabet' lhses))
+
+let getNonNFPerm (lhses:string list):string list = 
+
+    let rec getPerm (ls:string) (res:string list):string list =
+        match ls with
+        | "" -> res
+        | _  -> if (not (String.exists ((=) '$') ls) && ls.Length > 1) 
+                then 
+                    getPerm (ls.[1..] + "$" + char(ls.[0]).ToString()) (res @ [ls.[1..] + "$" + char(ls.[0]).ToString()]) 
+                else 
+                    if 
+                        (ls.[1] = '$') 
+                    then 
+                        res 
+                    else 
+                        getPerm (ls.[1..] + char(ls.[0]).ToString()) (res @ [ls.[1..] + char(ls.[0]).ToString()])
+
+    let rec getNonNFPerm' (ls:string list):string list =
+        match ls with
+        | []    -> []
+        | x::xs -> getPerm x (getNonNFPerm' xs)
+
+    Set.toList (set (getNonNFPerm' (List.filter (fun x -> String.length x > 1) lhses)))
+
+let getLabels (lhses:string list):string list = 
+
+    let rec getLabels' (ls:string list):string list =
+        match ls with
+        | []    ->  []
+        | x::xs ->  if String.length x > 1
+                    then
+                        [for i in 1..(String.length x) do yield x.Substring(0,i)] @ getLabels' xs
+                    else
+                        x :: getLabels' xs
+    
+    let res = Set.toList (set (getLabels' lhses))
+    List.filter (fun x -> not (List.exists (fun x' -> x' = x + "$") res)) res
+
+let matchA' (s:string) (a:string list) (b:string list):string =
+    if      List.exists (fun x -> s = x) b          then "b" + s
+    elif    List.exists (fun x -> s + "$" = x) b    then ("b" + s + "$")
+    elif    List.exists (fun x -> s = x) a          then "a" + s
+    else    "e1"
+
+let rec matchA (s:string) (a:string list) (b:string list) (non:string list):string = 
+    if      List.exists (fun x -> s.Substring(0, (String.length s) - 1) = x) non then s.Substring(0, (String.length s)-1)
+    elif    List.exists (fun x -> s = x) a  then "a" + s
+    elif    String.length s > 1             then matchA (s.Substring(1)) a b non
+    else    "e1"
+
+let rec matchB (s:string) (a:string list) (b:string list) (non:string list) (nonPerm:string list):string =
+    if List.exists (fun x -> s.Substring(0, (String.length s) - 1) = x) nonPerm then s.Substring(0, (String.length s)-1)
+    elif List.exists (fun x -> s = x) b then "b" + s
+    elif List.exists (fun x -> s + "$" = x) b    then ("b" + s + "$")
+    else "" 
+
+let rec checkPref (s:string) (pref:string):bool =
+    match s with
+    | ""    -> false
+    | _ when s = pref -> true
+    | _ -> checkPref (s.Substring(0, ((String.length s) - 1))) pref
+
+let build (lhses:string list):state list = 
+
+    let alph = getAlphabet (lhses)
+
+    let nonNFPerm = getNonNFPerm(lhses)
+
+    let labA = getLabels(lhses)
+
+    let labB = getLabels(nonNFPerm)
+
+    []
---
+++ NonTerm/Program.fs
@@ -0,0 +1,35 @@
+﻿open Microsoft.Z3
+open NonNF
+
+(* 
+let ctx = new Context()
+
+let x = ctx.MkTrue()
+let y = ctx.MkFalse()
+
+let z1 = ctx.MkConst("z1", ctx.MkBoolSort())
+let z2 = ctx.MkConst("z2", ctx.MkBoolSort())
+
+let expr1 = ctx.MkEq(x, y)
+
+let expr2 = ctx.MkAnd([ x ; y ] |> List.toArray)
+
+let expr3 = ctx.MkEq(z1, x)
+
+let solver = ctx.MkSolver()
+*)
+
+let xs = [ "a"; "a"; "a"; "b"; "abc"; "bca" ]
+let ys = []
+
+[<EntryPoint>]
+let main argv = 
+    (*solver.Assert(expr3)
+    if solver.Check() = Status.SATISFIABLE then printfn "SAT\n" else printfn "UNSAT\n"
+    let m = solver.Model
+    for d in m.Decls do
+        System.Console.WriteLine("{0} -> {1}", d.Name, m.ConstInterp(d))
+        *)
+    printfn "%A" (checkPref "ab" "ab")
+    System.Console.ReadKey() |> ignore
+    0 // return an integer exit code
